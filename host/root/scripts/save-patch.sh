c1e6488c1e208694c2451d10552141ac404c9a25
Critter & Guitari <critter@critterandguitari.com>
2017-05-01 02:30:47 +0000

    small fix, v1.3

--- host/root/scripts/save-patch.sh
+++ host/root/scripts/save-patch.sh
@@ -8,7 +8,7 @@ oscsend localhost 4001 /enableauxsub i 1
 # clear aux screen
 oscsend localhost 4001 /oled/aux/clear i 1
 oscsend localhost 4001 /oled/aux/line/1 s "Saving..."
-oscsend localhost 4000 /saveStateNew i 1
+oscsend localhost 4000 /saveState i 1
 
 # set to aux screen, signals screen update
 oscsend localhost 4001 /oled/setscreen i 1
