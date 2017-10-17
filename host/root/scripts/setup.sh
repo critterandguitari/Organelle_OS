cf352a26127346eb1163af8000cec2a9e52b30d9
Mark Harris <mharris@technobear.com>
2017-10-17 17:33:45 +0200

    remove misspelt  scaling_govenor line
    setting scaling_governor is not required, since performance is the default/boot up mode of the kernel
    (this line was not doing anything since it was also misspelt ;) )

--- host/root/scripts/setup.sh
+++ host/root/scripts/setup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 sudo systemctl stop serial-getty@ttymxc0.service
 sudo dmesg -n 1
-sudo echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_govenor
 amixer set PCM 192-
 amixer set PCM 170+
 amixer set Headphone 127-
