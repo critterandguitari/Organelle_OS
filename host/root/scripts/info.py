0da3faf3acfc971e87eadaddeee04c841587eb2e
Owen Osborn <owen@critterandguitari.com>
2017-12-15 19:10:55 +0000

    add ssid to info script

--- host/root/scripts/info.py
+++ host/root/scripts/info.py
@@ -17,10 +17,11 @@ print "loading og module"
 # imports
 current_dir = os.path.dirname(os.path.abspath(__file__))
 og = imp.load_source('og', current_dir + '/og.py')
+wifi = imp.load_source('wifi_control', current_dir + '/wifi_control.py')
 
 # returns output if exit code 0, NA otherwise
 def run_cmd(cmd) :
-    ret = 'NA'
+    ret = 'None'
     try:
         ret = subprocess.check_output(['bash', '-c', cmd], close_fds=True)
     except: pass
@@ -42,7 +43,9 @@ og.start_app()
 # get info
 cpu = "CPU: ..."
 usbdrive = "USB Drive: " + run_cmd("grep usbdrive /proc/mounts | awk '{print $1}' | sed -e 's/\/dev\///'")
-midi_dev = "MIDI Dev: " + run_cmd("aplaymidi -l | awk '{if (NR==2) print $2}'")
+midi_dev =  run_cmd("aplaymidi -l | awk '{if (NR==2) print $2}'")
+if (midi_dev == ""): midi_dev = 'None'
+midi_dev = "MIDI Dev: " + midi_dev
 version = run_cmd("cat " + fw_dir + "/version")
 build_tag = run_cmd("cat " + fw_dir + "/buildtag")
 version = "Version: " + version + build_tag
@@ -52,13 +55,20 @@ patch = "  " + run_cmd("ls /tmp/curpatchname")
 ip_address = "IP: " + socket.gethostbyname(socket.gethostname())
 host_name = "  " + run_cmd("ps aux | grep 'avahi.*running' | awk 'NR==1{print $13}' | sed 's/\[//' | sed 's/]//'")
 
+# check for wifi
+ssid = "not connected"
+if wifi.wifi_connected() :
+    ssid = wifi.current_net
+ssid = "  " + ssid
 
 #info.items = [usbdrive, midi_dev, version, "Patch Folder:", patch_dir, "User Root:", user_dir, ip_address, "Host Name:",host_name]
 info.items = [
 cpu,
 usbdrive, 
-midi_dev, 
+midi_dev,
 ip_address, 
+"WiFi Network:",
+ssid,
 "Host Name:",
 host_name,
 "Patch: ",
