071d340a0a53ec39c589eda1b407cbb7ada16d20
Owen Osborn <owenosborn@gmail.com>
2019-06-07 20:34:25 +0100

    wifi monitor updates every 2 seconds

--- platforms/organelle_m/fw_dir/scripts/wifi_monitor.py
+++ platforms/organelle_m/fw_dir/scripts/wifi_monitor.py
@@ -5,20 +5,11 @@ import os
 fw_dir = os.getenv("FW_DIR", "/root")
 wifi = imp.load_source('wifi_control', fw_dir + '/scripts/wifi_control.py')
 
-connected = False
 
 while True:
     if wifi.wifi_connected() :
-        tmp = True
+        os.system('oscsend localhost 4001 /wifiStatus i 1')
     else :
-        tmp = False
-    if tmp != connected :
-        connected = tmp
-        if connected :
-            print "connected"
-            os.system('oscsend localhost 4001 /wifiStatus i 1')
-        else :
-            print "not connected"
-            os.system('oscsend localhost 4001 /wifiStatus i 0')
-    time.sleep(1)
+        os.system('oscsend localhost 4001 /wifiStatus i 0')
+    time.sleep(2)
 
