f45ca47126109840ed1aafa51e8e74f0c405a38c
Owen Osborn <owen@critterandguitari.com>
2020-01-17 02:20:18 +0000

    fix ap mode for og 1

--- fw_dir/scripts/start-ap.sh
+++ fw_dir/scripts/start-ap.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 killall wpa_supplicant
 killall dhcpcd
-/root/scripts/create_ap --no-virt -n wlan0 Organelle coolmusic
+
+USER_DIR=$(cat /tmp/user_dir)
+
+AP_FILE="${USER_DIR}/ap.txt"
+
+if [ -f "$AP_FILE" ]; then
+    echo "$AP_FILE exists"
+    NET=$(head -n 1 $AP_FILE)
+    PW=$(tail -n 1 $AP_FILE)
+else 
+    echo "$AP_FILE does not exist, using default"
+    NET=Organelle
+    PW=coolmusic
+fi
+
+/root/scripts/create_ap --no-virt -n wlan0 $NET $PW
+
